19f7210d88cff528e0743299fe5fc9f0b9bc3595
mandimartins <mandimartins23@gmail.com>
2020-12-02 17:20:05 -0300

    Show coupon message

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link'
 import useSWR from 'swr'
 
 import SpeakerSvg from '../assets/svg/speaker.svg';
@@ -9,6 +10,12 @@ const Index = () => {
 
   const {data, error} =  useSWR('/api/get-promo',fetcher)
 
+  const showCouponMessage = (data) => {
+    return data.showCoupon && <h2 className="suggestion-section__subtitle">
+    {data.message}
+  </h2> 
+  }
+
   return (
     <main className="home">
       <section className="suggestion-section">
@@ -16,15 +23,13 @@ const Index = () => {
           O restaurante X sempre busca por atender melhor os seus clientes, por
           isso, estamos sempre abertos a ouvir a sua opinião.
         </h1>
-        <button className="suggestion-section__btn">
-          Dar opinião ou sugestão
-        </button>
-
-        {!data && <p>Carregando...</p>}
-        {data && <h2 className="suggestion-section__subtitle">
-          {data.message}
-          {/* Ao dar sua opinião e/ou sugestão ganhe 10% na sua próxima compra. */}
-        </h2> }
+        {!data ? <p>Carregando...</p> : showCouponMessage(data)}
+        <Link href="/pesquisa" >
+          <a className="suggestion-section__btn"> 
+             Dar opinião/sugestão
+          </a>
+        </Link>
+      
       </section>
       <SpeakerSvg className="speakerImage" />
     </main>
